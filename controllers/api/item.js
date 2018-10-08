b3c77ae2e6ab212be95ed9044b0adaacf047e53c
Stevan <stevanvasiljevic>
2018-10-08 14:28:03 +0200

    Item create

--- controllers/api/item.js
+++ controllers/api/item.js
@@ -9,7 +9,7 @@ var Items = require('../../collections/items');
 module.exports = function (router) {
 
     router.get('/read', function (req, res) {
-        var queryBuilder = knex.select('t1.id', 't1.name', 't1.description', 't1.id_user', 't1.id_currency','t1.price')
+        var queryBuilder = knex.select('t1.id', 't1.name', 't1.description', 't1.id_user', 't1.id_currency', 't1.price')
             .from('Item as t1')
             .where('t1.id_user', req.user.id)
         queryBuilder
@@ -23,6 +23,50 @@ module.exports = function (router) {
                     message: err.message
                 });
             });
+    });
+
+    router.post('/create', function (req, res) {
+        var data = req.body;
+
+        for (var key in data) {
+            if (data[key] === '') data[key] = null;
+        }
+        console.log('aaaaaaaaaaaaaaaaaaaa', req.user.id);
+
+        if (!data.id) {
+            Item.forge()
+                .save(Object.assign(data, {id_user:req.user.id}))
+                .then(function (newItem) {
+                    console.log('aaaaaaaaaaaaaaaaaaaa', newItem);
+                    res.status(200).json({
+                        data: newItem,
+                        success: true,
+                        message: "Item kreiran"
+                    });
+                })
+                .catch(function (err) {
+                    res.status(500).json({
+                        success: false,
+                        message: err.message
+                    });
+                });
+        } else {
+            Item.forge({ id: data.id })
+                .save(data, { patch: true, user: req.user })
+                .then(function (data) {
+                    res.status(200).json({
+                        data: data,
+                        success: true,
+                        message: "Item izmenjen"
+                    });
+                })
+                .catch(function (err) {
+                    res.status(500).json({
+                        success: false,
+                        message: err.message
+                    });
+                });
+        }
     });
 
 };
