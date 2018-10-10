b2af83cfacfaed083f5d22027eeab3570d5a99a6
boris91 <borisbabin91@gmail.com>
2018-10-10 14:13:23 +0200

    upload

--- controllers/api/item.js
+++ controllers/api/item.js
@@ -5,6 +5,10 @@ var knex = bookshelf.knex;
 var Item = require('../../models/item');
 var Items = require('../../collections/items');
 
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+var BluemixCOS = require('../../lib/BluemixCOS');
 
 module.exports = function (router) {
 
@@ -27,6 +31,7 @@ module.exports = function (router) {
 
     router.post('/create', function (req, res) {
         var data = req.body;
+        delete data.documents;
         for (var key in data) {
             if (data[key] === '') data[key] = null;
         }
@@ -82,4 +87,49 @@ module.exports = function (router) {
             });
     });
     /*******************************************/
+
+    router.post('/upload_picture', upload.single('document'), function (req, res, next) {
+        let filename, extension, bluemixObjectStorageFilename;
+        const parts = req.file.originalname.split('.');
+        var itemId = typeof req.body.itemId === 'object' ? req.body.itemId[0] : req.body.itemId;
+        console.log('wwwwwww ', req.file, req.body, itemId);
+        filename = parts[0];
+        if (parts.length > 1) {
+            extension = parts[1];
+        }
+        bluemixObjectStorageFilename = [filename, req.user.username, (new Date()).toISOString()].join('_');
+        bluemixObjectStorageFilename += '.' + extension;
+
+        knex.select('picture', 'id')
+            .from('Item')
+            .where('id', '=', itemId)
+            .then(function (result) {
+                BluemixCOS.doCreateObject({
+                    localBuffer: req.file.buffer,
+                    name: bluemixObjectStorageFilename
+                })
+                    .then(function (bluemixObjectStorageFile) {
+                        return Item.forge({ id: itemId })
+                            .save({
+                                picture: bluemixObjectStorageFilename,
+                            }, { patch: true });
+                    })
+                    .then((data) => {
+                        if (result[0].picture) {
+                            BluemixCOS.doDeleteObject(result[0].picture);
+                        }
+                        res.status(200).json({
+                            success: true,
+                            message: "Slika je uspešno uploadovana."
+                        });
+                    })
+                    .catch((err) => {
+                        console.log('errrrrrrrr',err);
+                        res.status(500).json({
+                            success: false,
+                            message: err.message
+                        });
+                    });
+            });
+    });
 };
